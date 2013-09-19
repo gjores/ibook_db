140af61bd8bb83f76623abed4e545533998734fa
Petter Gjöres <gjores@Petters-MacBook-Pro.local>
2013-09-19 20:11:47 +0200

    wtf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IbookDb::Application.config.secret_token = '3c93225e148f60f12b02e9132d76618d45a36c1288010bfc037ea86c9f6d068a14b7142ab1a99541721aeef28bdd05a0a000e74263ac46545e3fec63f927248f'
